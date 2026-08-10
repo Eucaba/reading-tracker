ca12ffae312cb0fe910486ee17e9cb822fb58b76
Eugenia Cames <eucaba.dev@gmail.com>
2026-08-10 18:14:15 +0200

    Initial commit

---
+++ reading-tracker/src/test/java/com/github/eucaba/reading_tracker/ReadingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eucaba.reading_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
