50fc40e1186ad71d39ae970aa9f91fb130ca17bb
Eugenia Cames <eucaba.dev@gmail.com>
2026-09-08 15:22:20 +0200

    Data Transfer Objects

--- reading-tracker/src/main/java/com/github/eucaba/reading_tracker/dto/BookDto.java
+++ reading-tracker/src/main/java/com/github/eucaba/reading_tracker/dto/BookDto.java
@@ -3,6 +3,7 @@ package com.github.eucaba.reading_tracker.dto;
 
 import com.github.eucaba.reading_tracker.domain.Author;
 import com.github.eucaba.reading_tracker.domain.BookFormat;
+import com.github.eucaba.reading_tracker.domain.BookGenre;
 import com.github.eucaba.reading_tracker.domain.Translator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,12 +13,14 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class BookDto {
+    private UUID id;
     private String title;
     private String isbn;
     private String publisher;
@@ -26,13 +29,15 @@ public class BookDto {
     private int pages;
     private List<Author> authors = new ArrayList<>();
     private List<Translator> translators = new ArrayList<>();
+    private List<BookGenre> genres = new ArrayList<>();
 
     //BookCreateForm
     public static BookDto test()
     {
         BookDto book = new BookDto();
 
-        // Populate book authors and translators with placeholders.
+        // Populate book authors, translators and genres
+        // with default placeholders.
         for (int i = 0; i < 5; i++)
         {
             Author author = new Author();
@@ -43,6 +48,12 @@ public class BookDto {
         {
             Translator translator = new Translator();
             book.translators.add(translator);
+        }
+
+        for (int k = 0; k < 3; k++)
+        {
+            BookGenre genre = new BookGenre();
+            book.genres.add(genre);
         }
 
         return book;
